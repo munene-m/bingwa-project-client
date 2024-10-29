986899a004147af084425a79d950ec946baa0f42
munene-m <munenenjue18@gmail.com>
2024-10-29 14:09:50 +0300

    chore: add icons for the customer table

---
+++ src/components/icons/TrashIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.5em"
+    height="1.5em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IcBaselineDelete',
+}
+</script>
